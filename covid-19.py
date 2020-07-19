adad2a7ce7a1979ca7d7a1c1bfd3e74501c55850
Gribesh Dhakal <gribeshdhakal@gmail.com>
2020-07-19 20:41:41 +0545

    defined method update

--- covid-19.py
+++ covid-19.py
@@ -2,3 +2,9 @@ import requests
 from win10toast import ToastNotifier
 import json
 import time
+
+def update():
+    r=requests.get('https://coronavirus-19-api.herokuapp.com/all')
+    data=r.json
+    text=f'Confirmed Cases :{data["cases"]} \nDeaths: {data["deaths"]} \nRecovered: {data["recovered"]}'
+    
